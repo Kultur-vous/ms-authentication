624bd69de4bd77c9158006edacc4be2547d999ed
marth154 <37938187+marth154@users.noreply.github.com>
2021-11-27 16:23:37 +0100

    Without env

--- db/clientMongo.ts
+++ db/clientMongo.ts
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose")
 require('dotenv').config();
-const mongoUri =
-  `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASS}@users.pnxzv.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+//const mongoUri =
+  //`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASS}@users.pnxzv.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+  const mongoUri =
+  `mongodb+srv://admin:CfQ8q6N59w6FHCt@users.pnxzv.mongodb.net/users?retryWrites=true&w=majority`;
   
 const uri = encodeURI(String(mongoUri));
 const mongoConnection = mongoose.connect(uri);
