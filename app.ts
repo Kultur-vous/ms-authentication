a8b5f04db41734709aad04e6a0750d0267aa6a8c
marth154 <37938187+marth154@users.noreply.github.com>
2021-12-01 14:45:40 +0100

    Heroku

--- app.ts
+++ app.ts
@@ -65,6 +65,6 @@ app.post("/sign-in", async (req: any, res: any) => {
   }
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server app listening on port 3000");
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server app listening on port 5000");
 });
